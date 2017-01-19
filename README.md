34f248455af0ca794b42e1867c3b01e3cf930e79
Jarek Rencz <jarek.rencz@functionite.com>
2017-01-19 17:53:32 +0100

    Add linters + fix errors, travis, Initial readme and bump node in nvmrc

---
+++ README.md
@@ -0,0 +1,6 @@
+# CSS theme starter [![Build Status][ci-img]][ci]
+
+CSS theme/framework/styleguide starter
+
+[ci-img]: https://travis-ci.org/jrencz/test-documentcss.svg
+[ci]: https://travis-ci.org/jrencz/test-documentcss
--- bs-config.js
+++ bs-config.js
@@ -1,12 +1,12 @@
 module.exports = {
-    server: true,
-    files: [
-        'styleguide',
-        'demos',
-    ],
-    port: 3333,
-    ui: {
-        port: 3334,
-    },
-    open: false,
+  server: true,
+  files: [
+    'styleguide',
+    'demos',
+  ],
+  port: 3333,
+  ui: {
+    port: 3334,
+  },
+  open: false,
 };
